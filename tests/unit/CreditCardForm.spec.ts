48d92171e459a1675ac31583d9ed0501f5dd2036
kysnrm <kysn.rm@gmail.com>
2020-03-29 20:05:10 +0900

    add test

--- tests/unit/CreditCardForm.spec.ts
+++ tests/unit/CreditCardForm.spec.ts
@@ -19,30 +19,78 @@ describe('Input', () => {
     const cardNumber = '4242424242424242'
     wrapper.setData({ cardNumber })
     expect((wrapper.vm as any).cardBrand).toBe('Visa')
+    expect((wrapper.vm as any).errorMessage).toBe('')
+  })
+  test('is invalid Visa', () => {
+    const cardNumber = '4242424242424224'
+    wrapper.setData({ cardNumber })
+    expect((wrapper.vm as any).errorMessage).toBe(
+      '有効なカード番号ではありません'
+    )
   })
   test('is Amex', () => {
     const cardNumber = '378282246310005'
     wrapper.setData({ cardNumber })
     expect((wrapper.vm as any).cardBrand).toBe('Amex')
+    expect((wrapper.vm as any).errorMessage).toBe('')
+  })
+  test('is invalid Amex', () => {
+    const cardNumber = '378282246310050'
+    wrapper.setData({ cardNumber })
+    expect((wrapper.vm as any).errorMessage).toBe(
+      '有効なカード番号ではありません'
+    )
   })
   test('is Diners', () => {
-    const cardNumber = '3056930009020004'
+    const cardNumber = '36227206271667'
     wrapper.setData({ cardNumber })
     expect((wrapper.vm as any).cardBrand).toBe('Diners')
+    expect((wrapper.vm as any).errorMessage).toBe('')
+  })
+  test('is invalid Diners', () => {
+    const cardNumber = '36227206271676'
+    wrapper.setData({ cardNumber })
+    expect((wrapper.vm as any).errorMessage).toBe(
+      '有効なカード番号ではありません'
+    )
   })
   test('is JCB', () => {
     const cardNumber = '3566002020360505'
     wrapper.setData({ cardNumber })
     expect((wrapper.vm as any).cardBrand).toBe('JCB')
+    expect((wrapper.vm as any).errorMessage).toBe('')
+  })
+  test('is invalid JCB', () => {
+    const cardNumber = '3566002020360550'
+    wrapper.setData({ cardNumber })
+    expect((wrapper.vm as any).errorMessage).toBe(
+      '有効なカード番号ではありません'
+    )
   })
   test('is Master', () => {
     const cardNumber = '5555555555554444'
     wrapper.setData({ cardNumber })
     expect((wrapper.vm as any).cardBrand).toBe('Master')
+    expect((wrapper.vm as any).errorMessage).toBe('')
+  })
+  test('is invalid Master', () => {
+    const cardNumber = '5555555555554445'
+    wrapper.setData({ cardNumber })
+    expect((wrapper.vm as any).errorMessage).toBe(
+      '有効なカード番号ではありません'
+    )
   })
   test('is Discover', () => {
     const cardNumber = '6011111111111117'
     wrapper.setData({ cardNumber })
     expect((wrapper.vm as any).cardBrand).toBe('Discover')
+    expect((wrapper.vm as any).errorMessage).toBe('')
+  })
+  test('is invalid Discover', () => {
+    const cardNumber = '6011111111111171'
+    wrapper.setData({ cardNumber })
+    expect((wrapper.vm as any).errorMessage).toBe(
+      '有効なカード番号ではありません'
+    )
   })
 })
